b14f4d137c3f31df49db595c04afff55b85a35b1
Lory-Stan TANASI <stantanasi@gmail.com>
2021-04-17 21:33:50 +0200

    First version made in 2019

---
+++ supp mais verifie/cv.php
@@ -0,0 +1,305 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+        <title>Lory-Stan TANASI</title>
+        
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width"/>
+        
+        <link rel="stylesheet" href="cv-style.css" />
+        
+        <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+    </head>
+    
+    <body>
+
+        <header>
+            
+            <img class="photo" src="cv-photo.jpg" alt="Ma photo de profil" />
+            
+            <div class="title">
+                
+                <h1>Lory-Stan TANASI-CHAMSOUDINE</h1>
+                
+                <span class="view" style="background: white; width: 90%;"></span>
+
+                <h2>Recherche de stage, domaine GEII</h2>
+                <h4>Génie Électrique et Informatique Industrielle</h4>
+                <h3>Du 13 avril 2020 au 26 juin 2020 (10 semaines)</h3>
+
+            </div>
+        
+        </header>
+        
+        <nav>
+
+            <section class="contact">
+                <h2>Contact</h2>
+
+                <p>19 ans</p>
+                <p><a href="tel:0651448793">06 51 44 87 93</a></p>
+                <p><a href="mailto:lorystan.tanasi@free.fr">lorystan.tanasi@free.fr</a></p>
+                <p>12 allée Youri Gagarine<br/>92300 - Levallois-Perret</p>
+            </section>
+
+            <section class="langues">
+                <h2>Langues</h2>
+                
+                <div class="level">
+                    <p>Anglais technique</p>
+                    <div class="progressbar"><span class="progress" style="width: 65%;"></span></div>
+                </div>
+                
+                <div class="level">
+                    <p>Espagnol</p>
+                    <div class="progressbar"><span class="progress" style="width: 20%;"></span></div>
+                </div>
+
+            </section>
+
+            <section class="competences">
+                <h2>Compétences</h2>
+
+                <div class="level">
+                    <p>Android Studio</p>
+                    <div class="progressbar"><span class="progress" style="width: 85%;"></span></div>
+                </div>
+                
+                <div class="level">
+                    <p>HTML5 / CSS3</p>
+                    <div class="progressbar"><span class="progress" style="width: 60%;"></span></div>
+                </div>
+                
+                <div class="level">
+                    <p>PHP / MySQL</p>
+                    <div class="progressbar"><span class="progress" style="width: 75%;"></span></div>
+                </div>
+                
+                <div class="level">
+                    <p>C</p>
+                    <div class="progressbar"><span class="progress" style="width: 65%;"></span></div>
+                </div>
+                
+                <div class="level">
+                    <p>Altium Designer</p>
+                    <div class="progressbar"><span class="progress" style="width: 60%;"></span></div>
+                </div>
+                
+                <div class="level">
+                    <p>e!COCKPIT - WAGO</p>
+                    <div class="progressbar"><span class="progress" style="width: 40%;"></span></div>
+                </div>
+                
+                <div class="level">
+                    <p>Pack Office</p>
+                    <div class="progressbar"><span class="progress" style="width: 95%;"></span></div>
+                </div>
+
+                <div class="level">
+                    <p>Unity Pro</p>
+                    <div class="progressbar"><span class="progress" style="width: 70%;"></span></div>
+                </div>
+
+            </section>
+
+        </nav>
+        
+        <table>
+            
+            <tr>
+                <td></td>
+
+                <td>
+                    <h2>Formation</h2>
+                </td>
+            </tr>
+
+            <tr>
+                <td>2018-2020</td>
+
+                <td>
+                    <section>
+                        <h3>
+                            Étudiant BAC+2 en DUT GEII
+                            <br/>
+                            Université Paris-Saclay – Cachan (94)
+                        </h3>
+                        <p>Génie Électrique et Informatique Industrielle – IUT de Cachan</p>
+                    </section>
+                </td>
+            </tr>
+
+            <tr>
+                <td>2018</td>
+
+                <td>
+                    <section>
+                        <h3>
+                            Diplôme du Baccalauréat Général série Scientifique
+                            <br/>
+                            Lycée Léonard de Vinci – Levallois-Perret (92)
+                        </h3>
+                        <p>Mention Assez Bien, option Sciences de l’Ingénieur, spécialité Informatique et Sciences du Numérique</p>
+                    </section>
+                </td>
+            </tr>
+            
+            
+            <tr>
+                <td></td>
+
+                <td>
+                    <span class="view"></span>
+                </td>
+            </tr>
+
+
+            <tr>
+                <td></td>
+
+                <td>
+                    <h2>Projets Personnels et Universitaires</h2>
+                </td>
+            </tr>
+            
+            <tr>
+                <td>2019</td>
+
+                <td>
+                    <section>
+                        <h3>Conception d’un robot guide commandé par Bluetooth</h3>
+                        <p>
+                            Conception d’une carte électronique sous Altium Designer
+                            <br/>
+                            Programmation d’un microcontrôleur sous Mbed
+                        </p>
+                    </section>
+                </td>
+            </tr>
+
+            <tr>
+                <td>2019</td>
+
+                <td>
+                    <section>
+                        <h3>Conception d’une voiture solaire pilotable à distance</h3>
+                        <p>Utilisation du compilateur Mbed, du logiciel e!COCKPIT et  d’Altium Designer</p>
+                    </section>
+                </td>
+            </tr>
+
+            <tr>
+                <td>2018-2019</td>
+
+                <td>
+                    <section>
+                        <h3>
+                            Conception d’une application Android permettant la gestion de livres (manga, BD…)
+                            <br/>
+                            Accessible sur <a href="http://lorystan.tanasi.free.fr/mangajap">MangaJap</a>
+                        </h3>
+                        <p>Programmation en java sous Android Studio, en SQL et en PHP</p>
+                    </section>
+                </td>
+            </tr>
+
+            <tr>
+                <td>2018</td>
+
+                <td>
+                    <section>
+                        <h3>Conception d’un robot suiveur de ligne</h3>
+                        <p>Programmation en langage C sous MPLAB<br/>Conception d’une carte électronique sous Altium Designer</p>
+                    </section>
+                </td>
+            </tr>
+
+            
+            <tr>
+                <td></td>
+
+                <td>
+                    <span class="view"></span>
+                </td>
+            </tr>
+
+
+            <tr>
+                <td></td>
+
+                <td>
+                    <h2>Expérience professionnelle</h2>
+                </td>
+            </tr>
+
+            <tr>
+                <td>Août 2018</td>
+
+                <td>
+                    <section>
+                        <h3>
+                            Agent Technique
+                            <br/>
+                            Centre Aquatique - Levallois-Perret (92)
+                        </h3>
+                        <p>Nettoyage et entretien de la piscine municipale (vestiaires, bureaux, escaliers), Gestion des déchets, Vitrerie</p>
+                    </section>
+                </td>
+            </tr>
+
+            
+            <tr>
+                <td></td>
+
+                <td>
+                    <span class="view"></span>
+                </td>
+            </tr>
+
+
+            <tr>
+                <td></td>
+
+                <td>
+                    <h2>Centres d'intérêts</h2>
+                </td>
+            </tr>
+
+            <tr>
+                <td></td>
+
+                <td>
+                    <section>
+                        <h3>Loisirs</h3>
+                        <p>Développement Android, Musique, Robotique, Programmation</p>
+                    </section>
+                </td>
+            </tr>
+
+            <tr>
+                <td></td>
+
+                <td>
+                    <section>
+                        <h3>Voyages</h3>
+                        <p>Martinique, Crète, Canada, Royaume-Uni, Espagne, Croatie, Corse, Tenerife, Sardaigne, Les Landes, Hautes-Alpes</p>
+                    </section>
+                </td>
+            </tr>
+
+            <tr>
+                <td></td>
+
+                <td>
+                    <section>
+                        <h3>Sport</h3>
+                        <p>Musculation, Natation</p>
+                    </section>
+                </td>
+            </tr>
+
+        </table>
+
+    </body>
+	
+</html>

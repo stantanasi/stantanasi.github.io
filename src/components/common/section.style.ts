cfcccee183d01c55db5247b5b6682f85477eb5b1
LoryStanTanasi <lory-stan.tanasi@42c.fr>
2021-10-24 14:14:27 +0200

    Update margin and padding

--- src/components/common/section.style.ts
+++ src/components/common/section.style.ts
@@ -1,13 +1,12 @@
 import styled from 'styled-components';
 
 export const SectionWrapper = styled.section`
-  margin-bottom: 100px;
   scroll-margin-top: 100px;
-  padding: 0 5%;
+  padding: 100px 5% 100px;
 `;
 
 export const SectionHeader = styled.h2`
-  margin: 100px auto;
+  margin: 0 auto 100px;
   font-size: 1.5rem;
   font-weight: 900;
   text-align: center;

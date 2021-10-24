877ba9f67be232fd74e932222f708d7a566306a6
LoryStanTanasi <lory-stan.tanasi@42c.fr>
2021-10-24 14:13:31 +0200

    Replace my profile pic image
    Add center of interest inside About me

--- src/components/about-me/about-me.style.tsx
+++ src/components/about-me/about-me.style.tsx
@@ -7,19 +7,36 @@ export const AboutMeWrapper = styled(SectionWrapper)`
 export const AboutMeInfo = styled.article`
   display: flex;
   align-items: center;
-
-  > div:first-child {
-    margin-right: 10%;
-  }
+  gap: 2rem;
 
   @media (max-width: 768px) {
     flex-direction: column;
-
-    > div:first-child {
-      margin-right: 0;
-      margin-bottom: 30px;
-    }
   }
 `;
 
-export const AboutMeSummary = styled.p``;
+export const AboutMeAvatar = styled.img`
+  width: 230px;
+`;
+
+export const AboutMeSummary = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+export const Story = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+`
+
+export const Quote = styled.div`
+  text-align: center;
+  font-style: italic;
+`
+
+export const Interests = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 3rem;
+`;
--- src/components/about-me/about-me.tsx
+++ src/components/about-me/about-me.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { SectionHeader } from '../common/section.style'
-import AboutMeAvatar from './about-me-avatar'
-import { AboutMeInfo, AboutMeSummary, AboutMeWrapper } from './about-me.style'
+import * as Styled from './about-me.style'
 
 interface Props {
 
@@ -9,24 +8,40 @@ interface Props {
 
 const AboutMe = (props: Props) => {
   return (
-    <AboutMeWrapper id="about-me">
+    <Styled.AboutMeWrapper id="about-me">
       <SectionHeader>À propos</SectionHeader>
-      <AboutMeInfo>
-        <AboutMeAvatar src="/images/lory-stan-tanasi.jpg" />
+      <Styled.AboutMeInfo>
+        <Styled.AboutMeAvatar src="/images/me.png" alt="Avatar" />
 
-        <AboutMeSummary>
-          Bonjour, je suis Lory-Stan TANASI, développeur Android et web.
-          <br />
-          Diplômé de l'IUT de Cachan en Génie Électrique et Informatique Industrielle, suivie d'une année à l'ESTIAM en alternance chez 42c.
-          <br />
-          <br />
-          Autodidacte et motivé, je réalise depuis 2018, ma propre <a href="https://play.google.com/store/apps/details?id=com.tanasi.mangajap">application Android disponible sur le Play Store</a>. Durant la création, j'ai également réalisé le <a href="https://mangajap.000webhostapp.com/">site web</a> ainsi que l'<a href="https://mangajap.000webhostapp.com/api/manga">API</a>.
-          <br />
-          <br />
-          Visitez mon GitHub si vous souhaitez regarder en détails certains de mes projets.
-        </AboutMeSummary>
-      </AboutMeInfo>
-    </AboutMeWrapper>
+        <Styled.AboutMeSummary>
+          <Styled.Story>
+            <p>
+              Diplômé de l'IUT de Cachan en Génie Électrique et Informatique Industrielle, suivie d'une année à l'ESTIAM en alternance chez 42c.
+            </p>
+            <p>
+              Autodidacte et motivé, je réalise depuis 2018, ma propre <a href="https://play.google.com/store/apps/details?id=com.tanasi.mangajap">application Android disponible sur le Play Store</a>, ainsi que l'<a href="https://mangajap.000webhostapp.com/api/manga">API</a> et le <a href="https://mangajap.000webhostapp.com/">site web</a>.
+            </p>
+            <p>
+              Visitez mon GitHub si vous souhaitez regarder en détails certains de mes projets.
+            </p>
+          </Styled.Story>
+          <Styled.Quote>
+            <p>"Déterminé à atteindre le top"</p>
+            <p>— Stan</p>
+          </Styled.Quote>
+
+          <div>
+            <h3>Centres d'intérêts</h3>
+            <Styled.Interests>
+              <div>Musique</div>
+              <div>Voyage</div>
+              <div>Films</div>
+              <div>Sport</div>
+            </Styled.Interests>
+          </div>
+        </Styled.AboutMeSummary>
+      </Styled.AboutMeInfo>
+    </Styled.AboutMeWrapper>
   )
 }
 

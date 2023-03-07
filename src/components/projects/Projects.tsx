ce744586b5a7379b95d1b2a12709858264fcfc3a
Lory-Stan TANASI <stantanasi@gmail.com>
2023-03-07 21:53:27 +0100

    feat(projects): styling using Bootstrap

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,53 +1,37 @@
-import axios from 'axios';
-import { Component } from 'react'
+import React, { useEffect, useState } from 'react';
+import { Container, Row } from 'react-bootstrap';
 import { GitHubProject } from '../../types/GitHub';
-import { SectionHeader } from '../common/Section.style'
 import Project from './Project';
-import * as Styled from './Projects.style';
 
-interface ProjectsState {
-  gitHubProjects: GitHubProject[];
-  mangajapProjects: GitHubProject[];
-}
+export default function Projects() {
+  const [projects, setProjects] = useState<GitHubProject[]>([])
 
-export default class Projects extends Component<{}, ProjectsState> {
-  state: ProjectsState = {
-    gitHubProjects: [],
-    mangajapProjects: [],
-  }
+  useEffect(() => {
+    setProjects([])
 
-  componentDidMount() {
-    axios.get<GitHubProject[]>("https://api.github.com/users/stantanasi/repos?sort=pushed&direction=desc")
-      .then(response => response.data)
-      .then(response => this.setState({
-        gitHubProjects: response,
-      }));
-    axios.get<GitHubProject[]>("https://api.github.com/orgs/mangajap/repos?sort=pushed&direction=desc")
-      .then(response => response.data)
-      .then(response => this.setState({
-        mangajapProjects: response,
-      }));
-  }
+    fetch('https://api.github.com/users/stantanasi/repos?sort=pushed&direction=desc')
+      .then((response) => response.json())
+      .then((data: GitHubProject[]) => setProjects((prev) => prev.concat(data)))
+    fetch('https://api.github.com/orgs/mangajap/repos?sort=pushed&direction=desc')
+      .then((response) => response.json())
+      .then((data: GitHubProject[]) => setProjects((prev) => prev.concat(data)))
+  }, [])
 
-  render() {
-    return (
-      <Styled.ProjectWrapper id="projects">
-        <SectionHeader>Projets</SectionHeader>
-        <Styled.GitHubProjectsWrapper>
-          {this.state.gitHubProjects.map((project, index) => (
-            <Project
-              key={index}
-              project={project} />
-          ))}
-        </Styled.GitHubProjectsWrapper>
-        <Styled.GitHubProjectsWrapper>
-          {this.state.mangajapProjects.map((project, index) => (
-            <Project
-              key={index}
-              project={project} />
-          ))}
-        </Styled.GitHubProjectsWrapper>
-      </Styled.ProjectWrapper>
-    )
-  }
+  return (
+    <section
+      id="projects"
+      className="py-3 bg-light m-0"
+    >
+      <Container className="py-5">
+        <h2 className="display-4 pb-5 text-center">Projets</h2>
+        <Row>
+          {projects
+            .sort((a, b) => new Date(b.pushed_at).getTime() - new Date(a.pushed_at).getTime())
+            .map((project, index) => (
+              <Project key={index} project={project} />
+            ))}
+        </Row>
+      </Container>
+    </section>
+  )
 }

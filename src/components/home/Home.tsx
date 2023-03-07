fcfa9b50e87fd07cdbd9a3d64ebe16f521622d03
Lory-Stan TANASI <stantanasi@gmail.com>
2023-03-07 23:07:10 +0100

    feat(home): styling using Bootstrap

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,34 +1,59 @@
-import React from 'react'
-import { Button } from '../common/Button'
-import * as Styled from './Home.style'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import React from 'react';
+import { Button, Container } from 'react-bootstrap';
 
-const Home = (props: {}) => {
+export default function Home() {
   return (
-    <Styled.HomeWrapper id="home">
-      <Styled.HomeSummary>
-        <Styled.HomeWelcome>Bonjour, je suis</Styled.HomeWelcome>
-        <Styled.HomeName>Lory-Stan TANASI</Styled.HomeName>
-        <Styled.HomeJobOccuped>Développeur mobile Android et web</Styled.HomeJobOccuped>
-
-        <Styled.HomeButtons>
-          <Button as="a" href="https://docs.google.com/document/d/1lE_4oHBLQV5tKlT6t3OKqG20fIo8HiGoXfMDn7dER8M">Télécharger mon CV</Button>
-        </Styled.HomeButtons>
-
-        <Styled.HomeSocial>
-          <a href="https://github.com/stantanasi" target="__blank">
-            <Styled.HomeIcon src="/images/social/github.svg" alt="GitHub" />
+    <Container
+      id="home"
+      as="section"
+      className="min-vh-100 d-flex align-content-center align-items-center"
+      fluid
+    >
+      <Container className="text-center">
+        <h1 className="display-1">
+          Lory-Stan TANASI
+        </h1>
+        <h2 className="lead typist">
+          Développeur mobile Android et web
+        </h2>
+        <div className="p-5 d-flex gap-4 justify-content-center">
+          <a
+            className="text-dark"
+            href="https://github.com/stantanasi"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faGithub} size="3x" />
           </a>
-          <a href="https://www.linkedin.com/in/lory-stan-tanasi" target="__blank">
-            <Styled.HomeIcon src="/images/social/linkedin.svg" alt="LinkedIn" />
+          <a
+            className="text-dark"
+            href="https://www.linkedin.com/in/lory-stan-tanasi"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faLinkedin} size="3x" />
           </a>
-        </Styled.HomeSocial>
-      </Styled.HomeSummary>
-
-      <Styled.HomeImage>
-        <img src="/images/developer.png" alt="" />
-      </Styled.HomeImage>
-    </Styled.HomeWrapper>
+          <a
+            className="text-dark"
+            href="https://www.instagram.com/stan.tns"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faInstagram} size="3x" />
+          </a>
+        </div>
+        <Button
+          href="https://docs.google.com/document/d/1lE_4oHBLQV5tKlT6t3OKqG20fIo8HiGoXfMDn7dER8M"
+          variant="outline-dark"
+          size="lg"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Télécharger mon CV
+        </Button>
+      </Container>
+    </Container>
   )
 }
-
-export default Home

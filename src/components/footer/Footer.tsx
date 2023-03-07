ae35d2038a60bccec0585b38abbc5d5e9c63a2ba
Lory-Stan TANASI <stantanasi@gmail.com>
2023-03-07 23:08:32 +0100

    feat(footer): styling using Bootstrap

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,36 +1,57 @@
-import React from 'react'
-import { SectionHeader } from '../common/Section.style'
-import * as Styled from './Footer.style'
+import { faGithub, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { Container } from 'react-bootstrap';
 
-const Footer = (props: {}) => {
+export default function Footer() {
   return (
-    <>
-      <Styled.ContactWrapper id="contact">
-        <SectionHeader>Me contacter</SectionHeader>
-        <div>
-          Email : <a href="mailto:stantanasi@gmail.com">stantanasi@gmail.com</a>
-        </div>
-      </Styled.ContactWrapper>
-      <Styled.FooterWrapper>
-        <Styled.FooterDetails>
-          <h2>Lory-Stan TANASI</h2>
-          <span>© {new Date().getFullYear()} Lory-Stan TANASI. All rights reserved</span>
-        </Styled.FooterDetails>
+    <footer
+      id="contact"
+      className="mt-auto py-5 text-center"
+      style={{ backgroundColor: '#F5F5F5' }}
+    >
+      <Container>
+        <h2 className="display-4 pb-5 text-center">
+          Me contacter
+        </h2>
+        <p className="lead text-center pb-3">
+          <p>
+            <a className="text-decoration-none" href="mailto:stantanasi@gmail.com">
+              stantanasi@gmail.com
+            </a>
+          </p>
+          <p className="d-flex gap-4 justify-content-center">
+            <a
+              className="text-dark"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/stantanasi"
+            >
+              <FontAwesomeIcon icon={faGithub} size="2x" />
+            </a>
+            <a
+              className="text-dark"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.linkedin.com/in/lory-stan-tanasi"
+            >
+              <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            </a>
+            <a
+              className="text-dark"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.instagram.com/stan.tns"
+            >
+              <FontAwesomeIcon icon={faInstagram} size="2x" />
+            </a>
+          </p>
+        </p>
 
-        <Styled.FooterSocial>
-          <a href="https://github.com/stantanasi" target="__blank">
-            <img src="/images/icons/github.svg" alt="GitHub" width="24" />
-          </a>
-          <a href="https://www.linkedin.com/in/lory-stan-tanasi" target="__blank">
-            <img src="/images/icons/linkedin.svg" alt="LinkedIn" width="24" />
-          </a>
-          <a href="https://www.instagram.com/stan.tns" target="__blank">
-            <img src="/images/icons/instagram.svg" alt="Instagram" width="24" />
-          </a>
-        </Styled.FooterSocial>
-      </Styled.FooterWrapper>
-    </>
+        <div className="pt-4">
+          <em>© {new Date().getFullYear()} Lory-Stan TANASI. Tous droits réservés</em>
+        </div>
+      </Container>
+    </footer>
   )
 }
-
-export default Footer

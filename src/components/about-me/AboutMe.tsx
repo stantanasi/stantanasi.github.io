37ba7499caaf01e9d41cec37aefed2055c988415
Lory-Stan TANASI <stantanasi@gmail.com>
2023-03-07 21:19:13 +0100

    feat(about-me): styling using Bootstrap

--- src/components/about-me/AboutMe.tsx
+++ src/components/about-me/AboutMe.tsx
@@ -1,48 +1,30 @@
-import React from 'react'
-import { SectionHeader } from '../common/Section.style'
-import * as Styled from './AboutMe.style'
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 
-interface Props {
-
-}
-
-const AboutMe = (props: Props) => {
+export default function AboutMe() {
   return (
-    <Styled.AboutMeWrapper id="about-me">
-      <SectionHeader>À propos</SectionHeader>
-      <Styled.AboutMeInfo>
-        <Styled.AboutMeAvatar src="/images/me.png" alt="Avatar" />
-
-        <Styled.AboutMeSummary>
-          <Styled.Story>
-            <p>
-              Diplômé de l'IUT de Cachan en Génie Électrique et Informatique Industrielle, suivie d'une année à l'ESTIAM en alternance chez 42c.
-            </p>
-            <p>
-              Autodidacte et motivé, je réalise depuis 2018, ma propre <a href="https://play.google.com/store/apps/details?id=com.tanasi.mangajap">application Android disponible sur le Play Store</a>, ainsi que l'<a href="https://mangajap.000webhostapp.com/api/manga">API</a> et le <a href="https://mangajap.000webhostapp.com/">site web</a>.
-            </p>
-            <p>
-              Visitez mon GitHub si vous souhaitez regarder en détails certains de mes projets.
-            </p>
-          </Styled.Story>
-          <Styled.Quote>
-            <p>"Déterminé à atteindre le top"</p>
-            <p>— Stan</p>
-          </Styled.Quote>
-
-          <div>
-            <h3>Centres d'intérêts</h3>
-            <Styled.Interests>
-              <div>Musique</div>
-              <div>Voyage</div>
-              <div>Films</div>
-              <div>Sport</div>
-            </Styled.Interests>
-          </div>
-        </Styled.AboutMeSummary>
-      </Styled.AboutMeInfo>
-    </Styled.AboutMeWrapper>
+    <Container
+      id="about-me"
+      as="section"
+      className="py-3"
+      fluid
+      style={{ backgroundColor: "#E9ECEF" }}
+    >
+      <Row className="p-5">
+        <Col xs={5} className="d-none d-lg-block align-self-center">
+          <img
+            className="border border-secondary rounded-circle"
+            src="./images/me.png"
+            width="375"
+            height="375"
+            alt="Me"
+          />
+        </Col>
+        <Col lg={7}>
+          <h2 className="display-4 mb-5 text-center">À propos</h2>
+          <p className="lead text-center">Je m'appelle Lory-Stan TANASI. Visitez mon GitHub si vous souhaitez regarder en détails certains de mes projets.</p>
+        </Col>
+      </Row>
+    </Container>
   )
 }
-
-export default AboutMe

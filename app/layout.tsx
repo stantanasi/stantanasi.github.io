c55c12bf2663743247e8525bc410012100947808
Lory-Stan TANASI <stantanasi@gmail.com>
2026-04-07 21:15:11 -0400

    feat(app): display navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from '@/components/ui/Navbar';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v16-appRouter';
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -30,6 +31,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <AppRouterCacheProvider>
+          <Navbar />
           {children}
         </AppRouterCacheProvider>
       </body>

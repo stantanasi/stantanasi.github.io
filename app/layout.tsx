72a69bdb1798fa80ee61e72c80625cbe1b62721c
Lory-Stan TANASI <stantanasi@gmail.com>
2026-04-08 09:50:03 -0400

    feat: uninstall Tailwind CSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,9 +27,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable}`}
     >
-      <body className="min-h-full flex flex-col">
+      <body>
         <AppRouterCacheProvider>
           <Navbar />
           {children}

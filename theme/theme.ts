57d286e31174f55f8889b1c7fc384d00deb5e49b
Lory-Stan TANASI <stantanasi@gmail.com>
2026-04-09 21:15:04 -0400

    fix(theme): force dark mode

--- theme/theme.ts
+++ theme/theme.ts
@@ -3,6 +3,12 @@
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: { main: '#c8f060' },
+    background: { default: '#000', paper: '#000' },
+    text: { primary: '#ffffff' },
+  },
   components: {
     MuiStack: {
       defaultProps: {
